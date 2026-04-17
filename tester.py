ac4aa060fbeb9bc4ff319d3c8e44e379a2ae2c6d
shafey kamran <slashmonkey0@gmail.com>
2026-04-17 17:41:23 +0500

    Refactor task input and add_task function
    Refactored task input handling and file operations.

--- tester.py
+++ tester.py
@@ -1,42 +1,31 @@
+from asyncio import tasks
 import json
 import os
 
-if os.path.exists("Tasks.json"):
-    pass
-else:
-    file=  open("Tasks.json", "w")
-    file.close()
 
-class task:
-        def __init__(self,id, name, description, created_date):
-            self.id = id
-            self.name = name
-            self.description = description
-            self.created_date = created_date
-            self.updated_date= "NULL"
-        def input_task():
-            id= input("Enter task id: ")
-            name= input("Enter task name: ")
-            description= input("Enter task description: ")
-            created_date= input("Enter task created date: ")
-            return task(id, name, description, created_date)
+def input_task(name):
+    task=[]
+   
+    id= input("Enter task id: ")
+    task.append(int(id))
+    task.append(name)
+    description= input("Enter task description: ")
+    task.append(description)
+    created_date= input("Enter task created date: ")
+    task.append(created_date)
+    return task
+    
 def add_task(words):
     if os.path.exists("Tasks.json"):
         pass
     else:
-        file=  open("Tasks.json", "w")
-        file.close()
-    task= task.input_task()
-    words.pop(0)
-    str1=" ".join(words)
+        with open("Tasks.json", "w") as file:
+            json.dump([], file)
+    task=input_task(words[1])
     file= open("Tasks.json", "r")
-    try:
-        data= json.load(file)
-    except json.JSONDecodeError:
-        data = []
-    data.append(str1)
+    data=json.load(file)
+    data.append(task)
     file.close()
-    
     file = open("Tasks.json", "w")
     json.dump(data, file)
     file.close()
