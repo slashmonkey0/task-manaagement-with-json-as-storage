db72720571275120fc98bab98d6b3f1ca9269aa2
shafey kamran <slashmonkey0@gmail.com>
2026-04-19 14:40:05 +0500

    Refactor task management and add new features
    Refactor task management functions and enhance functionality.

--- tester.py
+++ tester.py
@@ -2,21 +2,31 @@ import json
 import os
 from datetime import datetime
 
-def input_task(name):
+def making_task(description):
     task=[]
     with open("Tasks.json", "r") as file:
-        data=json.load(file)
-    for number in range(0,200):
-        if number in data:
+        try:
+            data=json.load(file)
+        except json.JSONDecodeError:
+            data = {}
+    id_found=False
+
+    for id in range(1,200):
+        if id in data:
             continue
         else:
+            id_found=True
             break
-    task.append(number)
-    task.append(name)
-    description= input("Enter task description: ")
+    if not(id_found):
+        print("No ids available")
+        return None
+
+    task.append(id)
     task.append(description)
-    created_date=datetime.today().strftime("%Y-%m-%d")
+    created_date=datetime.today().strftime("%y-%m-%d")
+    updated_date=datetime.today().strftime("%y-%m-%d")
     task.append(created_date)
+    task.append(updated_date)
     task.append("not done")
     return task
     
@@ -27,16 +37,16 @@ def add_task(words):
         with open("Tasks.json", "w") as file:
             json.dump({}, file)
 
-    task=input_task(words[1])
+    task=making_task(words[1])
     with open("Tasks.json", "r") as file:
         try:
             data=json.load(file)
         except json.JSONDecodeError:
             data = {}
     data[task[0]] = {
-        "name":task[1],
-        "description":task[2],
-        "Created_Date":task[3],
+        "description":task[1],
+        "Created_Date":task[2],
+        "updated_date":task[3],
         "Status":task[4]
     }
     with open("Tasks.json", "w") as file:
@@ -48,22 +58,81 @@ def list_tasks():
     for key, value in data.items():
         print(f"{key}: {value}")
 
-def mark(id,status):
+def list_tasks_todo():
+    file=open("Tasks.json","r")
+    data=json.load(file)  
+    for key, value in data.items():
+        if value["Status"]=="in-progress":
+            print(f"{key}: {value}")
+
+def list_tasks_done():
+    file=open("Tasks.json","r")
+    data=json.load(file)  
+    for key, value in data.items():
+        if value["Status"]=="done":
+            print(f"{key}: {value}")
+    
+def list_tasks_todo():
+    file=open("Tasks.json","r")
+    data=json.load(file)  
+    for key, value in data.items():
+        if value["Status"]=="todo":
+            print(f"{key}: {value}")
+        
+def mark_task(id,status):
     with open("Tasks.json", "r") as file:
         data=json.load(file)
-    #data[id][""]
-    
+    if id in data:
+        data[id]["Status"]=status
+        data[id]["updated_date"]=datetime.today().strftime("%y-%m-%d")
+    else:
+        print("Task not found")
+        return None
+    with open("Tasks.json", "w") as file:
+        json.dump(data,file)
+
+def delete_task(id):
+    with open("Tasks.json","r") as file:
+        data= json.load(file)
+    if id in data:
+        del data[id]
+    else:
+        print("Task not found")
+        return None
+    with open("Tasks.json","w") as file:
+        json.dump(data,file)
+
+def update_task(id,description):
+    with open("Tasks.json","r") as file:
+        data=json.load(file)
+    if id in data:
+        data[id]["description"]=description
+        data[id]["updated_date"]=datetime.today().strftime("%y-%m-%d")
+    else:
+        print("Task not found")
+        return None
+    with open("Tasks.json","w") as file:
+        json.dump(data,file)
 
 while True:
     inputU= input("task_cli ")
     words= inputU.split()
-    print(words)
     if words[0] == "add":
         add_task(words)
     elif words[0] == "list":
         list_tasks()
+    elif words == "list in-progress":
+        list_tasks_todo()
+    elif words== "list done":
+        list_tasks_done()
+    elif words == "list todo":
+        list_tasks_todo()
+    elif words[0]=="delete":
+        delete_task(words[1])
     elif words[0] == "mark":
-        mark(words[1],words[2])
+        mark_task(words[1],words[2])
+    elif words[0] == "update":
+        update_task(words[1],words[2])
     elif words[0] == "exit":
         break
         
